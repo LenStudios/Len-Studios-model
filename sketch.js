de99b18358a5041320eb988f9048e88a0f8020be
LenStudios <159979802+LenStudios@users.noreply.github.com>
2024-02-15 10:57:14 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,2 +1,2 @@
-<model-viewer camera-controls touch-action="pan-y" alt="A 3D model of a sphere" src="../../shared-assets/models/reflective-sphere.gltf">
+<model-viewer camera-controls touch-action="pan-y" alt="A 3D model of a sphere" src="https://github.com/LenStudios/Len-Studios-model/blob/main/whole-trying.gltf">
 </model-viewer>
