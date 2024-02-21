0b9425af3d09373eae39fe63b7421b8bd0eea133
LenStudios <159979802+LenStudios@users.noreply.github.com>
2024-02-21 20:18:15 +1100

    Update script.js

--- script.js
+++ script.js
@@ -12,36 +12,13 @@ const onProgress = (event) => {
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
 
-var elem = document.getElementById('myCanvas'),
-    elemLeft = elem.offsetLeft,
-    elemTop = elem.offsetTop,
-    context = elem.getContext('2d'),
-    elements = [];
+var elem = document.querySelector('model-viewer'),
+
 
 // Add event listener for `click` events.
 elem.addEventListener('click', function(event) {
-    var x = event.pageX - elemLeft,
-        y = event.pageY - elemTop;
-    console.log(x, y);
-    elements.forEach(function(element) {
-        if (y > element.top && y < element.top + element.height && x > element.left && x < element.left + element.width) {
+
             alert('clicked an element');
-        }
     });
 
-}, false);
-
-// Add element.
-elements.push({
-    colour: '#05EFFF',
-    width: 150,
-    height: 100,
-    top: 20,
-    left: 15
-});
-
-// Render elements.
-elements.forEach(function(element) {
-    context.fillStyle = element.colour;
-    context.fillRect(element.left, element.top, element.width, element.height);
-});
+};
